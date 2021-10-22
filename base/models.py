ad0d03f933803a7ac630f47cdd021c7c6c8a4893
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-22 12:24:10 +0300

    first initialization

---
+++ base/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    first_name = models.CharField(max_length=200, null=True, blank=True)
+    last_name = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+            return str(self.user)
